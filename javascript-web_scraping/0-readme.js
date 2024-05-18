16f270dc0441664e50029b03dab8667c9869925f
aishalieva0 <aysalieva6@gmail.com>
2024-05-19 02:22:08 +0400

    Readme

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFileSync(filePath, 'utf-8', (err, data) => {
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
