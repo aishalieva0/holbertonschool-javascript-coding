d0754fa51aad43f44fbc4d5b8bb57107b5543c6a
aishalieva0 <aysalieva6@gmail.com>
2024-05-19 12:44:14 +0400

    Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const { json } = require('stream/consumers');
+
+const movieId = process.argv[2];
+const endpoint = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(endpoint, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
